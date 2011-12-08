4577c5611914993a5408a9f2753e03deb0591040
igor.bread@gmail.com <igor.bread@gmail.com@b9c272d9-d23a-85a5-8ee8-dd98d5299530>
2011-12-08 21:24:13 +0000

    Alguns bugfixes feitos pela manhã e não commitados + sugestão da professora durante a apresentação.

--- TesteServidor/src/controle/UsuarioControle.java
+++ TesteServidor/src/controle/UsuarioControle.java
@@ -1,5 +1,14 @@
 package controle;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.TreeSet;
 
@@ -8,84 +17,173 @@ import modelo.Usuario;
 public class UsuarioControle {
 	private HashMap<Integer, Usuario> usuarios;
 	private int ultimoId;
-	
+
+	private File arquivo;
+
 	private static UsuarioControle instanciaUnica = null;
-	
-	private UsuarioControle() {
+
+	private UsuarioControle() {		
+		arquivo = new File("./userData.dat");
 		usuarios = new HashMap<Integer, Usuario>();
 		ultimoId = 0;
+		
+		try {
+			if (arquivo.exists()) {
+				FileInputStream fstream = new FileInputStream(arquivo);
+				DataInputStream in = new DataInputStream(fstream);
+				BufferedReader br = new BufferedReader(new InputStreamReader(in));
+				String linha;
+				
+				linha = br.readLine();
+				ultimoId = Integer.parseInt(linha);
+				
+				linha = br.readLine();
+				int nUsers = Integer.parseInt(linha);
+				
+				for (int i = 0; i < nUsers; ++i) {
+					linha = br.readLine();
+					int id = Integer.parseInt(linha);
+					
+					criaUsuario(id);
+					
+					linha = br.readLine();
+					int nVideos = Integer.parseInt(linha);
+					
+					for (int j = 0; j < nVideos; ++j) {
+						linha = br.readLine();
+						insereVideoGostado(id, linha, false);
+					}
+					
+					linha = br.readLine();
+					nVideos = Integer.parseInt(linha);
+					
+					for (int j = 0; j < nVideos; ++j) {
+						linha = br.readLine();
+						insereVideoNaoGostado(id, linha, false);
+					}
+				}
+
+				in.close();
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	public static UsuarioControle getInstance() {
 		if (instanciaUnica == null) {
 			instanciaUnica = new UsuarioControle();
 		}
 		return instanciaUnica;
 	}
-	
+
 	public TreeSet<String> getVideosRecomendadosFromId(int id) {
-		if ( !existeCliente(id) ) {
+		if (!existeCliente(id)) {
 			criaUsuario(id);
 		}
-		
-		return usuarios.get(id).getVideosRecomendados();		
+
+		return usuarios.get(id).getVideosRecomendados();
 	}
-	
+
 	public TreeSet<String> getVideosFromId(int id) {
-		if ( !existeCliente(id) ) {
+		if (!existeCliente(id)) {
 			criaUsuario(id);
 		}
-		
-		return usuarios.get(id).getVideosGostados();		
+
+		return usuarios.get(id).getVideosGostados();
 	}
-	
+
 	public void criaUsuario(int id) {
 		usuarios.put(id, new Usuario(id));
+		ultimoId = Math.max(ultimoId, id);
 	}
-	
-	public void insereVideoNaoGostado(int id, String video) {
-		if ( !existeCliente(id) ) {
+
+	public void insereVideoNaoGostado(int id, String video, boolean grava) {
+		if (!existeCliente(id)) {
 			criaUsuario(id);
 		}
-		
+
 		usuarios.get(id).acrescentaVideoNaoGostado(video);
+		
+		if (grava) {
+			gravaDados();
+		}
 	}
-	
-	public void insereVideoGostado(int id, String video) {
-		if ( !existeCliente(id) ) {
+
+	public void insereVideoGostado(int id, String video, boolean grava) {
+		if (!existeCliente(id)) {
 			criaUsuario(id);
 		}
+
+		usuarios.get(id).acrescentaVideoGostado(video);
 		
-		boolean recalcula = usuarios.get(id).acrescenteVideoGostado(video);
-		
-		if(recalcula) {
-			recalculaRatings(id);
+		if (grava) {
+			gravaDados();
 		}
 	}
-	
+
 	public int geraId() {
 		ultimoId++;
 		return ultimoId;
 	}
-	
+
 	public boolean existeCliente(int id) {
-		if ( !usuarios.containsKey(id) ) {
+		if (!usuarios.containsKey(id)) {
 			return false;
 		}
 		return true;
 	}
-	
-	public void recalculaRatings(int id) {
-		TreeSet<Integer> ids = new TreeSet<Integer>();
-		ids.addAll(usuarios.keySet());
-		
-		Usuario recalculado = usuarios.get(id);
-		
-		for (Integer idOutro : ids) {
-			if(idOutro != id) {
-				Usuario outro = usuarios.get(idOutro);
-				recalculado.calculaRating(outro);
+
+	public void recalculaRatings() {
+		for (Integer id : usuarios.keySet()) {
+			
+			usuarios.get(id).setTopId(0);
+			usuarios.get(id).setTopRating(-100);
+			TreeSet<Integer> ids = new TreeSet<Integer>();
+			ids.addAll(usuarios.keySet());
+
+			for (Integer idOutro : ids) {
+				if (idOutro != id) {
+					usuarios.get(id).calculaRating(usuarios.get(idOutro));
+				}
 			}
+		}
+	}
+
+	public void metodoDaThread() {
+		System.out.println("Recalculando distâncias.");
+		recalculaRatings();
+	}
+	
+	public void gravaDados() {
+		System.out.println("Gravando dados.");
+		try {
+			FileWriter outFile = new FileWriter(arquivo);
+			PrintWriter out = new PrintWriter(outFile);
+			
+			out.println(ultimoId);
+			out.println(usuarios.size());
+			for (Integer id1 : usuarios.keySet()) {
+				out.println(id1);
+				out.println(usuarios.get(id1).getVideosGostados().size());
+				for (String str : usuarios.get(id1).getVideosGostados()) {
+					out.println(str);
+				}
+				out.println(usuarios.get(id1).getVideosNaoGostados().size());
+				for (String str : usuarios.get(id1).getVideosNaoGostados()) {
+					out.println(str);
+				}
+			}
+			
+			out.flush();
+			out.close();
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
--- TesteServidor/src/modelo/Usuario.java
+++ TesteServidor/src/modelo/Usuario.java
@@ -1,8 +1,8 @@
 package modelo;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.TreeSet;
-
 
 public class Usuario {
 
@@ -11,116 +11,123 @@ public class Usuario {
 	private TreeSet<String> videosNaoGostados;
 	private TreeSet<String> videosRecomendados;
 	private HashMap<Integer, Double> ratings;
-	
+
 	private int topId;
 	private double topRating;
-	
-	private int countVideos;
-	
+
 	public Usuario(int id) {
 		this.id = id;
 		videosGostados = new TreeSet<String>();
 		videosNaoGostados = new TreeSet<String>();
 		videosRecomendados = new TreeSet<String>();
 		ratings = new HashMap<Integer, Double>();
-		countVideos = 0;
 	}
-	
+
 	public int getId() {
 		return this.id;
 	}
-	
+
 	public TreeSet<String> getVideosGostados() {
 		return this.videosGostados;
 	}
-	
-	public boolean acrescenteVideoGostado(String link) {
+
+	public void acrescentaVideoGostado(String link) {
 		videosGostados.add(link);
-		
-		countVideos++;
-		if (countVideos < 5) {
-			return false;
-		} else {
-			countVideos = 0;
-			topId = 0;
-			topRating = 0.0;
-			return true;
-		}
 	}
-	
+
 	public TreeSet<String> getVideosRecomendados() {
 		return this.videosRecomendados;
 	}
-	
+
 	public void acrescenteVideoRecomendado(String link) {
-		videosRecomendados.add(link);		
+		videosRecomendados.add(link);
 	}
-	
+
 	public TreeSet<String> getVideosNaoGostados() {
 		return this.videosNaoGostados;
 	}
-	
+
 	public void acrescentaVideoNaoGostado(String link) {
 		videosNaoGostados.add(link);
 	}
-	
+
 	public void calculaRating(Usuario outro) {
 		int idOutro = outro.getId();
-		
+
 		TreeSet<String> videosGostadosOutro = outro.getVideosGostados();
 		int countGostados = 0;
 		TreeSet<String> videosPossiveisRecomendados = new TreeSet<String>();
 		int countDiferenca = 0;
-		
+		int countOpostos = 0;
+
 		for (String gostado : videosGostadosOutro) {
-			if ( videosGostados.contains(gostado) ) {
+			if (videosGostados.contains(gostado)) {
 				countGostados++;
+			} else if (videosNaoGostados.contains(gostado)) {
+				countOpostos++;
 			} else {
-				countDiferenca++;
 				videosPossiveisRecomendados.add(gostado);
 			}
 		}
-		
+
 		TreeSet<String> videosNaoGostadosOutro = outro.getVideosNaoGostados();
 		int countNaoGostados = 0;
-		
+
 		for (String naogostado : videosNaoGostadosOutro) {
-			if ( videosNaoGostados.contains(naogostado) ) {
+			if (videosNaoGostados.contains(naogostado)) {
 				countNaoGostados++;
+			} else if (videosGostados.contains(naogostado)) {
+				countOpostos++;
 			}
 		}
-		
-		int totalVideos = videosGostados.size() + videosNaoGostados.size() + videosGostadosOutro.size() + videosNaoGostadosOutro.size();
-		
-		double rating = countGostados*0.4 + countNaoGostados*0.4 + countDiferenca*0.2;
+
+		HashSet<String> todosOsVideos = new HashSet<String>();
+		todosOsVideos.addAll(videosGostados);
+		todosOsVideos.addAll(videosNaoGostados);
+		todosOsVideos.addAll(videosGostadosOutro);
+		todosOsVideos.addAll(videosNaoGostadosOutro);
+
+		int totalVideos = todosOsVideos.size();
+		countDiferenca = totalVideos - countGostados - countNaoGostados - countOpostos;
+
+		double rating = countGostados * 0.5 + countNaoGostados * 0.5
+				- countDiferenca * 0.1 - countOpostos * 0.5;
 		rating /= totalVideos;
-		
+
 		if (rating > topRating) {
 			topRating = rating;
 			topId = idOutro;
 			escolheVideosRecomendados(videosPossiveisRecomendados);
 		}
-		
+
 		ratings.put(idOutro, rating);
 	}
-	
+
 	public int getMelhorRatingId() {
 		return topId;
 	}
-	
+
 	public void escolheVideosRecomendados(TreeSet<String> videosPossiveis) {
 		videosRecomendados.clear();
-		
-		if(videosPossiveis.size() <= 5) {
+
+		if (videosPossiveis.size() <= 5) {
 			videosRecomendados.addAll(videosPossiveis);
 		} else {
 			String[] array = (String[]) videosPossiveis.toArray();
-			
-			for(int i = 0; i < 5; i++) {
-				int aleatorio = (int) ( (Math.random() * array.length) );
-				
-				videosRecomendados.add( array[aleatorio] );
+
+			for (int i = 0; i < 5; i++) {
+				int aleatorio = (int) ((Math.random() * array.length));
+
+				videosRecomendados.add(array[aleatorio]);
 			}
 		}
+	}
+
+	public void setTopRating(int tr) {
+		topRating = tr;
+	}
+
+	public void setTopId(int ti) {
+		topId = ti;
 	}
 }
